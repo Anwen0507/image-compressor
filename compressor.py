1f2289842bcf883dbb39daaec30dd4870e04f091
Anwen Hao <haoanwen@gmail.com>
2025-12-21 01:47:44 -0800

    Implemented low rank approximation

--- compressor.py
+++ compressor.py
@@ -10,3 +10,14 @@ def compress(image_path, k):
     # Computer Singular Value Decomposition
     # As noted, libraries use bidiagonalization and QR decomposition to optimize
     U, S, Vt = np.linalg.svd(A, full_matrices=False)
+
+    # Truncate to rank k
+    U_k = U[:, :k]
+    S_k = np.diag(S[:k])
+    Vt_k = Vt[:k, :]
+
+    # Reconstruct the compressed image
+    A_k = np.dot(U_k, np.dot(S_k, Vt_k))
+
+    return A, A_k, U_k, S_k, Vt_k
+
