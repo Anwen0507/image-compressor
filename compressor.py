5836f5f4fbfb9e15b0f403506bd498e87ba058c6
Anwen Hao <haoanwen@gmail.com>
2025-12-21 01:45:55 -0800

    Compute SVD using numpy

--- compressor.py
+++ compressor.py
@@ -6,3 +6,7 @@ def compress(image_path, k):
     # Load the image and convert to grayscale matrix A
     img = Image.open(image_path).convert('L')
     A = np.array(img)
+
+    # Computer Singular Value Decomposition
+    # As noted, libraries use bidiagonalization and QR decomposition to optimize
+    U, S, Vt = np.linalg.svd(A, full_matrices=False)
